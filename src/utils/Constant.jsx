4360fc42f43ea1bfb5e5a00d871e898f81e0897b
Ayush <heyayush87@gmail.com>
2025-06-24 17:21:32 +0530

    added backend live url

--- src/utils/Constant.jsx
+++ src/utils/Constant.jsx
@@ -1,4 +1,2 @@
 export const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000"
-    : import.meta.env.VITE_API_URL || "https://devtinder-noyb.onrender.com";
+  import.meta.env.VITE_API_URL || "https://devtinder-noyb.onrender.com";
